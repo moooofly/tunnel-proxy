df58097d4f58f0d7484874862184b52e4682409a
moooofly <centos.sf@gmail.com>
2019-02-13 16:33:55 +0800

    feat: basic auth configuration support from both file and CLI

--- services/basic/goproxy_basic.go
+++ services/basic/goproxy_basic.go
@@ -11,25 +11,30 @@ import (
 	"github.com/elazarl/goproxy/ext/auth"
 	"github.com/moooofly/goproxy/services"
 	"github.com/moooofly/goproxy/utils"
+	"github.com/moooofly/goproxy/utils/authx"
 )
 
 const realm = "LLS-Realm"
 
 type BasicArgs struct {
-	Local *string
-	White *string
+	Local    *string
+	White    *string
+	AuthFile *string
+	Auth     *[]string
 }
 
 type Basic struct {
-	cfg    BasicArgs
-	log    *logger.Logger
-	isStop bool
+	cfg       BasicArgs
+	basicAuth authx.BasicAuth
+	log       *logger.Logger
+	isStop    bool
 }
 
 func NewBasic() services.Service {
 	return &Basic{
-		cfg:    BasicArgs{},
-		isStop: false,
+		cfg:       BasicArgs{},
+		basicAuth: authx.BasicAuth{},
+		isStop:    false,
 	}
 }
 
@@ -37,10 +42,11 @@ func (s *Basic) StopService() {
 	defer func() {
 		e := recover()
 		if e != nil {
-			s.log.Printf("stop basic proxy crashed,%s", e)
+			s.log.Printf("Stop basic proxy crashed, %s", e)
 		} else {
 			s.log.Printf("service basic proxy stopped")
 		}
+		s.basicAuth = authx.BasicAuth{}
 		s.cfg = BasicArgs{}
 		s.log = nil
 		s = nil
@@ -51,6 +57,13 @@ func (s *Basic) StopService() {
 func (s *Basic) Start(args interface{}, log *logger.Logger) (err error) {
 	s.log = log
 	s.cfg = args.(BasicArgs)
+
+	err = s.InitBasicAuth()
+	if err != nil {
+		return
+	}
+
+	//s.basicAuth.Show()
 
 	wl, err := utils.LoadWhiteList(*s.cfg.White)
 	if err != nil {
@@ -74,8 +87,8 @@ func (s *Basic) Start(args interface{}, log *logger.Logger) (err error) {
 			proxy.OnRequest(conds...).HandleConnect(goproxy.AlwaysReject)
 
 			// basic auth
-			proxy.OnRequest().Do(auth.Basic(realm, verify))
-			proxy.OnRequest().HandleConnect(auth.BasicConnect(realm, verify))
+			proxy.OnRequest().Do(auth.Basic(realm, s.Verify))
+			proxy.OnRequest().HandleConnect(auth.BasicConnect(realm, s.Verify))
 
 			err = http.ListenAndServe(addr, proxy)
 			if err != nil {
@@ -90,7 +103,26 @@ func (s *Basic) Clean() {
 	s.StopService()
 }
 
-// FIXME(moooofly): should be more general
-func verify(user, passwd string) bool {
-	return user == "foo" && passwd == "bar"
+func (s *Basic) InitBasicAuth() (err error) {
+	s.basicAuth = authx.NewBasicAuth(s.log)
+
+	if *s.cfg.AuthFile != "" {
+		n, err := s.basicAuth.AddFromFile(*s.cfg.AuthFile)
+		if err != nil {
+			return err
+		}
+		s.log.Printf("auth data added from file %d, total:%d", n, s.basicAuth.Total())
+	}
+
+	if len(*s.cfg.Auth) > 0 {
+		n := s.basicAuth.Add(*s.cfg.Auth)
+		s.log.Printf("auth data added from CLI %d, total:%d", n, s.basicAuth.Total())
+	}
+
+	return
+}
+
+func (s *Basic) Verify(user, passwd string) bool {
+	return s.basicAuth.Check(user, passwd)
+
 }
---
+++ utils/authx/basic_auth.go
@@ -0,0 +1,75 @@
+package authx
+
+import (
+	"io/ioutil"
+	logger "log"
+	"strings"
+
+	"github.com/moooofly/goproxy/utils/mapx"
+)
+
+type BasicAuth struct {
+	data mapx.ConcurrentMap
+	log  *logger.Logger
+}
+
+func NewBasicAuth(log *logger.Logger) BasicAuth {
+	return BasicAuth{
+		data: mapx.NewConcurrentMap(),
+		log:  log,
+	}
+}
+
+func (ba *BasicAuth) AddFromFile(file string) (n int, err error) {
+	ct, err := ioutil.ReadFile(file)
+	if err != nil {
+		return
+	}
+	pairs := strings.Split(strings.Replace(string(ct), "\r", "", -1), "\n")
+	for _, pair := range pairs {
+		if strings.HasPrefix(pair, "#") {
+			continue
+		}
+		u := strings.Split(strings.Trim(pair, " "), ":")
+		if len(u) == 2 {
+			ba.data.Set(u[0], u[1])
+			n++
+		}
+	}
+	return
+}
+
+func (ba *BasicAuth) Add(pairs []string) (n int) {
+	for _, pair := range pairs {
+		u := strings.Split(pair, ":")
+		if len(u) == 2 {
+			ba.data.Set(u[0], u[1])
+			n++
+		}
+	}
+	return
+}
+func (ba *BasicAuth) Delete(pair []string) {
+	for _, u := range pair {
+		ba.data.Remove(u)
+	}
+}
+
+func (ba *BasicAuth) Total() (n int) {
+	n = ba.data.Count()
+	return
+}
+
+func (ba *BasicAuth) Check(user, pass string) (ok bool) {
+	if got, ok := ba.data.Get(user); ok {
+		return got.(string) == pass
+	}
+	return false
+}
+
+func (ba *BasicAuth) Show() {
+	for i, key := range ba.data.Keys() {
+		value, _ := ba.data.Get(key)
+		ba.log.Printf("[%d] %s:%s\n", i, key, value)
+	}
+}
